1379db3e4147524e469812fe82300822fe4a66e6
unknown <ds@ESN501D1278484.educ501.local>
2025-09-19 17:00:09 -0300

    Aula 19-09

--- server.py
+++ server.py
@@ -19,6 +19,9 @@
 
 import os
 from http.server import SimpleHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
+
+filmesCadastrados = []
 
 class MyHandle (SimpleHTTPRequestHandler):
     def list_directory(self, path):
@@ -34,6 +37,17 @@ class MyHandle (SimpleHTTPRequestHandler):
             pass
         return super().list_directory(path)
     
+
+    def accont_user(self, login, senhaa):
+        loga = "rebeca@gmail.com"
+        senha = 1234
+
+        if login == loga and senha == senhaa:
+            return "Usuário logado"
+        else:
+            return "Usuário não existe"
+        
+
     def do_GET(self):
         if self.path == '/login':
             try:
@@ -75,7 +89,76 @@ class MyHandle (SimpleHTTPRequestHandler):
         else:
             super().do_GET()
 
+        if self.path == '/filmes':
+            html = "<html><body><h1>Filmes Cadastrados</h1><ul>"
+            for filme in filmesCadastrados:
+                html += f"<li><strong>{filme['Nome do filme']}</strong> - {filme['Diretor']} ({filme['Ano que foi lançado']})</li>"
+            html += "</ul></body></html>"
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(html.encode('utf-8'))
+
+        else:
+            super().do_GET()
+                
+
+    def do_POST(self):
+        if self.path == '/send_login':
+            content_length = int(self.headers['Content-length'])
+            body = self.rfile.read(content_length).decode('utf-8')
+            form_data = parse_qs(body)
+
+            login = form_data.get('email', [''])[0]
+            senha = int(form_data.get('senha', [''])[0])
+
+            logou = self.accont_user(login,senha)
+
+            print("Data form:")
+            print("Email: ", form_data.get('email', [''])[0])
+            print("Password: ", form_data.get('senha', [''])[0])
+
+
+            self.send_response (200)
+            self.send_header ("Content-type", "text/html")
+            self.end_headers ()
+            self.wfile.write ("Data Retrieving Sucess!".encode('utf-8'))
+            self.wfile.write(logou.encode('utf-8'))
+
+        elif self.path == '/send_cadastrofilmes':
+            content_length = int(self.headers['Content-length'])
+            body = self.rfile.read(content_length).decode('utf-8')
+            form_datafilme = parse_qs(body)
+
+            nomeFilme = form_datafilme.get('nome', [''])[0]
+            atoresFilme = form_datafilme.get('atores', [''])[0]
+            diretorFilme = form_datafilme.get('diretores', [''])[0]
+            anoFilme = form_datafilme.get('ano', [''])[0]
+            generoFilme = form_datafilme.get('genero', [''])[0]
+            produtoraFilme = form_datafilme.get('produtora', [''])[0]
+            sinopseFilme = form_datafilme.get('sinopse', [''])[0]
+
+            filminhos = {
+                'Nome do filme ' : nomeFilme,
+                'Atores do filme ' : atoresFilme,
+                'Diretor ' : diretorFilme,
+                'Ano que foi lançado ' : anoFilme,
+                'Genero do filme ' : generoFilme,
+                'Produtora ' : produtoraFilme,
+                'Sinopse do filme ' : sinopseFilme
+            }
+
+            filmesCadastrados.append(filminhos)
             
+
+            self.send_response (200)
+            self.send_header ('location', '/filmes')
+            self.end_headers ()
+            self.wfile.write("Filme cadastrado com sucesso!!!!!".encode('utf-8'))
+            
+        else:
+            super(MyHandle, self).do_POST()
     
 def main ():
     server_address = ('', 8000)
